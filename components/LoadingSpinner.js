ca4667ed61584934bdc2ebbe728b931715d0975d
Chris Raser <chris@dreadedmonkeygod.net>
2023-09-04 11:01:49 -0700

    MAIN: added loading spinner

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -1,7 +1,19 @@
 import {Fragment} from "react";
+import {ColorRing} from "react-loader-spinner";
+import styles from './LoadingSpinner.module.css';
 
 export default function LoadingSpinner(props) {
     return (
-        <Fragment>Loading...</Fragment>
+        <div className={styles.spinner}>
+            <ColorRing
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
+        </div>
     );
 }
