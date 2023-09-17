a609dd4d4f425e95f4473e133d79f7df9edb87eb
Chris Raser <chris@dreadedmonkeygod.net>
2023-09-17 11:04:38 -0700

    MAIN: google maps bug repro

--- pages/temp/index.js
+++ pages/temp/index.js
@@ -1,6 +1,25 @@
-import Annotation from "@/components/temp/Annotation";
-import AnnotationContainer from "@/components/temp/AnnotationContainer";
+/*
+ * Base Google Map example
+ */
 
-export default function TempIndex(props) {
-    return <AnnotationContainer />
+import GoogleMap from 'google-map-react';
+
+const markerStyle = {
+    padding: '25px',
+    backgroundColor: 'fuchsia',
+    borderRadius: '50%'
+}
+
+const Marker = () => <div style={markerStyle} />;
+
+export default function SampleGoogleMapsPage() {
+    return (
+        <div style={{ height: '100vh', width: '100vw' }}>
+            <GoogleMap
+                center={[59.938043, 30.337157]}
+                zoom={9}>
+                <Marker lat={59.955413} lng={30.337844} />
+            </GoogleMap>
+        </div>
+    );
 }
