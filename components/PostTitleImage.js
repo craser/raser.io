f19e044f84d586b681353d0c096371a45a196172
Chris Raser <chris@dreadedmonkeygod.net>
2023-10-06 18:44:49 -0700

    MAIN: Load postcards as title images

--- components/PostTitleImage.js
+++ components/PostTitleImage.js
@@ -3,10 +3,10 @@ import styles from "@/components/Post.module.css"
 export default function PostTitleImage(props) {
     let post = props.post;
 
-    if (post.Image_File_Name && (post.Image_File_Type == 'image')) {
+    if (post.imageFileName) {
         // FIXME: DO NOT COMMIT TO CODE REPOSITORY!
-        // TODO: Drop images into an AWS bucket somewhere.
-        let src = `http://dreadedmonkeygod.net/home/attachments/postcards/${post.Image_File_Name}`;
+        // TODO: somehow soft-code this url
+        let src = `http://localhost:8081/attachments/postcards/${post.imageFileName}`;
         return <img className="titleimage" src={src} />;
     } else {
         return null;
