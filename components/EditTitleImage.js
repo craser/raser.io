2f99c42e5810f827be7599b9623b700d79c83bd1
Chris Raser <chris@dreadedmonkeygod.net>
2023-11-30 17:36:52 -0800

    MAIN: styling the drop zone

--- components/EditTitleImage.js
+++ components/EditTitleImage.js
@@ -1,14 +1,27 @@
 import Dropzone from 'react-dropzone'
-
-
-
+import styles from './EditTitleImage.module.scss'
+import { useCallback, useState } from "react";
 
 export default function EditTitleImage({ post }) {
+    const [imgDataUrl, setImgDataUrl] = useState(null);
+    const onDrop = useCallback(acceptedFiles => {
+        console.log({ acceptedFiles });
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            console.log({ result: reader.result });
+            setImgDataUrl(reader.result);
+        });
+        reader.readAsDataURL(acceptedFiles[0]);
+    }, [])
+
+    console.log({ styles });
+    console.log({ imgDataUrl });
     return (
-        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-            {({getRootProps, getInputProps}) => (
-                <section>
-                    <div {...getRootProps()}>
+        <Dropzone onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
+                <section className={styles.dropzone}>
+                    {imgDataUrl && <img src={imgDataUrl} className={styles.titleimg}/>}
+                    <div {...getRootProps()} className={styles.verbiageContainer}>
                         <input {...getInputProps()} />
                         <p>Drag & drop some files here, or click to select files</p>
                     </div>
