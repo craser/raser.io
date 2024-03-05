e1d803d289102dc0828a3e9630c2c94b41e75cbc
Chris Raser <chris@dreadedmonkeygod.net>
2024-03-04 16:26:53 -0800

    AUTH: login working

--- components/auth/LoginModal.js
+++ components/auth/LoginModal.js
@@ -2,18 +2,55 @@ import Modal from "@/components/Modal";
 import styles from './LoginModal.module.scss'
 import { useEffect, useRef, useState } from "react";
 import { useAuthenticationContext } from "@/components/auth/AuthenticationContext";
+import { auth } from "mysql/lib/protocol/Auth";
+
+/**
+ *
+ * @param type
+ * @param ref
+ * @param placeholder
+ * @param defaultValue
+ * @param validate - function that either returns false if the field is valid, or an error string.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function LoginField({ type, placeholder, defaultValue, validate: validateValue, setValue }) {
+    const ref = useRef();
+    const [visited, setVisited] = useState(false);
+    let [errorMessage, setErrorMessage] = useState(null);
+
+
+    function validate() {
+        if (visited) {
+            let value = ref.current.valueOf().value;
+            setValue(value);
+            setErrorMessage(validateValue(value));
+        }
+    }
+
+    return <>
+        <input ref={ref} type={type} placeholder={placeholder} defaultValue={defaultValue} onChange={validate} onBlur={validate} onFocus={() => setVisited(true)}/>
+        {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+    </>;
+}
 
 export default function LoginModal({ onVisibilityChange }) {
     const authContext = useAuthenticationContext();
-    const userRef = useRef();
-    const passRef = useRef();
+    const [email, setEmail] = useState(authContext.getEmail);
+    const [pass, setPass] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
     const [visible, setVisible] = useState(!authContext.isAuthenticated);
 
+    function validateUserField(user) {
+        if (!user) {
+            return "required field";
+        } else {
+            return false;
+        }
+    }
+
     function onSubmit(e) {
         e.preventDefault();
-        const email = userRef.current.valueOf().value;
-        const pass = passRef.current.valueOf().value;
         authContext.login(email, pass)
             .then(() => setVisible(false))
             .catch(error => {
@@ -33,10 +70,21 @@ export default function LoginModal({ onVisibilityChange }) {
                 <form onSubmit={onSubmit}>
                     <div className={styles.logincontainer}>
                         <div className={styles.row}>
-                            <input ref={userRef} type="text" placeholder="email" defaultValue={authContext.getEmail()}/>
+                            <LoginField
+                                type="text"
+                                placeholder="email"
+                                defaultValue={email}
+                                validate={validateUserField}
+                                setValue={setEmail}
+                            />
                         </div>
                         <div className={styles.row}>
-                            <input ref={passRef} type="password" placeholder="password"/>
+                            <LoginField
+                                type="password"
+                                placeholder="password"
+                                validate={validateUserField}
+                                setValue={setPass}
+                            />
                         </div>
                         {errorMessage ? <div className={styles.error}>{errorMessage}</div> : null}
                         <div className={styles.row}>
