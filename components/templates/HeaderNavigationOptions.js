74fa11fea4cbc42743af6b5d3ab8c50f3fc4f44a
craser <chris@raser.io>
2024-04-02 09:48:08 -0700

    MAIN: quick fix to social links in header

--- components/templates/HeaderNavigationOptions.js
+++ components/templates/HeaderNavigationOptions.js
@@ -9,14 +9,11 @@ import AuthGuest from "@/components/auth/AuthGuest";
 export default function HeaderNavigationOptions(props) {
     return (
         <ul className={styles.sitenav}>
-            <li className="trailmaps-link">
-                <Link href={"trailmaps"}>trail maps</Link>
+            <li>
+                <a href="https://www.youtube.com/@burrito-fueledmtb9218">Burrito-Fueled MTB</a>
             </li>
-            <li className="twitter-link">
-                <a href="https://twitter.com/DeathB4Decaf">@DeathB4Decaf</a>
-            </li>
-            <li className="rss-link">
-                <Link href={"rss/monkeyfeed.xml"}>RSS</Link>
+            <li>
+                <a href="https://www.threads.net/@deathb4decaf">@DeathB4Decaf</a>
             </li>
             <AuthRecognized>
                 <LoginButton />
