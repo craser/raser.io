4f11eeaca518f4a7b727fbbf45d0ece47679a3f4
craser <chris@raser.io>
2024-06-28 23:08:08 -0700

    MAIN: Added redirects for /home/... so that archive links/redirects will still work.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/home/archive/:path*',
+        destination: '/archive/:path*',
+        permanent: true
+      },
+      {
+        source: '/home/:path*',
+        destination: '/',
+        permanent: true
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
