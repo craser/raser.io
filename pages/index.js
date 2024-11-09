1df8dd47c7e894eddc0ad216b9aa319b403834ac
craser <chris@raser.io>
2024-11-09 00:06:29 -0800

    MAIN: cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-//import { Inter } from 'next/font/google'
 import LogEntries from '@/components/LogEntries';
 import FrontPageLayout from "@/components/templates/FrontPageLayout";
 
 export default function Home() {
     return (
         <FrontPageLayout
-            content={<LogEntries pageSize={10}/>}
+            content={<LogEntries pageSize={20}/>}
         />
     )
 }
