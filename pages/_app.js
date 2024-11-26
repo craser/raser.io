658e35be16e061dc3f3b1c2b43b7a824509994ed
craser <chris@raser.io>
2024-11-26 13:02:38 -0800

    HONEYCOMB: initial add & init

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,23 @@ import SiteConfig from "@/lib/SiteConfig";
 import AnalyticsContext from "@/components/analytics/AnalyticsContext";
 import SearchProvider from "@/components/search/SearchProvider";
 import DataProvider from "@/components/api/DataProvider";
+import ObservabilityContext from "@/components/observability/ObservabilityContext";
 
 export default function App({ Component, pageProps }) {
     const statsigsdkKey = new SiteConfig().getValue('statsig.sdkKey');
     return (
-        <StatsigProvider sdkKey={statsigsdkKey} user={{ userID: "0000", email: "noone@nowhere.com" }}>
-            <AnalyticsContext>
-                <AuthenticationContext>
-                    <DataProvider>
-                        <SearchProvider>
-                            <Component {...pageProps} />
-                        </SearchProvider>
-                    </DataProvider>
-                </AuthenticationContext>
-            </AnalyticsContext>
-        </StatsigProvider>
+        <ObservabilityContext>
+            <StatsigProvider sdkKey={statsigsdkKey} user={{ userID: "0000", email: "noone@nowhere.com" }}>
+                <AnalyticsContext>
+                    <AuthenticationContext>
+                        <DataProvider>
+                            <SearchProvider>
+                                <Component {...pageProps} />
+                            </SearchProvider>
+                        </DataProvider>
+                    </AuthenticationContext>
+                </AnalyticsContext>
+            </StatsigProvider>
+        </ObservabilityContext>
     );
-
 }
