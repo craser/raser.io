5bfb19a6640ecd9ae1ddf0df3952d7424916c1cf
craser <chris@raser.io>
2024-12-10 13:54:11 -0800

    SEARCH: restored click

--- components/search/SearchResult.jsx
+++ components/search/SearchResult.jsx
@@ -4,10 +4,15 @@ import SearchResultMatchedText from "@/components/search/SearchResultMatchedText
 import { getPostLink } from "@/components/PostLink";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useSearchContext } from "@/components/search/SearchContext";
 
 export default function SearchResult({ terms, post, text }) {
+    const searchContext = useSearchContext();
     const router = useRouter();
-    const go = () => router.push(getPostLink(post));
+    const go = () => {
+        searchContext.showSearchUi(false);
+        router.push(getPostLink(post));
+    }
 
     return (
         <div className={styles.searchResult} onClick={go}>
--- components/search/SearchResultMatchedText.jsx
+++ components/search/SearchResultMatchedText.jsx
@@ -1,8 +1,33 @@
 import styles from './Search.module.scss'
 
 export default function SearchResultMatchedText({ terms, text }) {
+
+    const tokens = terms.split(/\s+/);
+    const splits = tokens.reduce((splits, token) => {
+        const s = [];
+        splits.forEach(split => {
+            for (let i = split.indexOf(token); i >= 0; i = split.indexOf(token, i)) {
+                s.push(split.substring(0, i));
+                s.push(token);
+                split = split.substring(i + token.length);
+            }
+        })
+        return s;
+    }, [text]);
+
+    const markedup = splits.map(split => {
+        if (tokens.indexOf(split) >= 0) {
+            return (<span class={styles.resultTokenMatch}>{split}</span>);
+        } else {
+            return split;
+        }
+    })
+
+
+
+
     return (
-        <div className={styles.searchResultMatchedText}>{text}</div>
+        <div className={styles.searchResultText}>{markedup}</div>
     )
 }
 
