5986e4f434b9771763a6d3b5d51518220278c999
craser <chris@raser.io>
2024-12-23 14:01:09 -0800

    SEARCH: basic functioning in place

--- __tests__/components/search/SearchImplementation.test.js
+++ __tests__/components/search/SearchImplementation.test.js
@@ -1,4 +1,4 @@
-import Search from '@/lib/search/SearchImplementation';
+import Search, { toRegex } from '@/lib/search/SearchImplementation';
 
 import MOCK_STUBS from './MockSearchStubs.json'; // 3 hard-coded mocks to test FUNCTIONALITY
 import SAMPLE_PROD_STUBS from './SampleSearchStubs.json'; // 1000 sample entries from prod to test PERFORMANCE
@@ -11,6 +11,37 @@ describe('Blog Post Search Implementation', () => {
         const results = search.search('commonth');
         expect(results.results.length == 1); // should only match one candidate
         expect(results.results[0].post.intro).toContain('commonth'); // candidate should actually match
+        expect(results.completion).toBe('ree');
+    });
+
+    test('Regex should always match input token', () => {
+        let token = 'common';
+        const re = toRegex(token);
+        expect(re.test(token)).toBe(true);
+    });
+
+    test('Should match all mock entries', () => {
+        const search = new Search(MOCK_STUBS);
+        const results = search.search('common');
+        expect(results.results.length).toBe(MOCK_STUBS.length);
+    });
+
+    test('Should match single word in searchable text', () => {
+        const STUB = {
+            "entryId": 103,
+            "datePosted": "2022-06-22T13:00:00.000+00:00",
+            "intro": "commonone",
+            "body": null
+        };
+        const search = new Search([STUB])
+        const results = search.search('common');
+        expect(results.results.length).toEqual(1);
+    })
+
+    test('Should recommend the most-used completion', () => {
+        const search = new Search(MOCK_STUBS);
+        const results = search.search('common');
+        expect(results.completion).toBe('one');
     });
 
     test('Search should get faster as more terms are added', () => {
