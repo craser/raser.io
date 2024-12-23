57cd28e925d7db9f9ec345fcec5fe23053836cfd
craser <chris@raser.io>
2024-12-23 11:32:02 -0800

    SEARCH: candidates now private to Search

--- __tests__/components/search/SearchImplementation.test.js
+++ __tests__/components/search/SearchImplementation.test.js
@@ -1,31 +1,25 @@
 import Search from '@/lib/search/SearchImplementation';
 
 import MOCK_STUBS from './MockSearchStubs.json'; // 3 hard-coded mocks to test FUNCTIONALITY
-import SAMPLE_STUBS from './SampleSearchStubs.json'; // 1000 sample entries from prod to test PERFORMANCE
+import SAMPLE_PROD_STUBS from './SampleSearchStubs.json'; // 1000 sample entries from prod to test PERFORMANCE
 
 
 describe('Blog Post Search Implementation', () => {
-    let search;
-
-    beforeEach(() => {
-        search = new Search();
-    });
-
 
     test('Should return only first entry, with "commonthree" as completion', () => {
-        const candidates = search.toSearchCandidates(MOCK_STUBS);
-        const results = search.search('commonth', candidates);
+        const search = new Search(MOCK_STUBS);
+        const results = search.search('commonth');
         expect(results.results.length == 1); // should only match one candidate
         expect(results.results[0].post.intro).toContain('commonth'); // candidate should actually match
     });
 
     test('Search should get faster as more terms are added', () => {
-        const candidates = search.toSearchCandidates(SAMPLE_STUBS);
+        const search = new Search(SAMPLE_PROD_STUBS);
         const tokens = ['mountain', 'bike', 'trails', 'corner'];
         //expect.assertions(tokens.length - 1); // one check per term after first
 
         let start = performance.now();
-        search.search(tokens[0], candidates);
+        search.search(tokens[0]);
         let baseline = performance.now() - start;
 
         const results = []; // { terms, elapsed }
@@ -33,7 +27,7 @@ describe('Blog Post Search Implementation', () => {
         for (let i = 2; i <= tokens.length; i++) {
             const terms = tokens.slice(0, i).join(' ');
             start = performance.now();
-            search.search(terms, candidates);
+            search.search(terms);
             let elapsed = performance.now() - start;
             results.push({ i, terms, elapsed });
             //expect(elapsed).toBeLessThan(baseline);
