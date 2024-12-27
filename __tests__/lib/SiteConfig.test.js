21c89d380c3e72e1459104a5ff4b26abf8091a6b
craser <chris@raser.io>
2024-12-26 23:06:58 -0800

    CONFIG: tests

--- __tests__/lib/SiteConfig.test.js
+++ __tests__/lib/SiteConfig.test.js
@@ -5,7 +5,7 @@ jest.mock('/siteconfig.json', () => ({
         postcard: 'https://example.com/images/postcards/{imageFileName}'
     },
     api: {
-        root: 'http://dummyhost.com',
+        root: '{NEXT_PUBLIC_API_ROOT}',
         endpoints: {
             entries: {
                 entry: '/fake-entry-endpoint/{id}'
@@ -14,17 +14,27 @@ jest.mock('/siteconfig.json', () => ({
     }
 }))
 
-test('get a formatted value', () => {
-    let value = new SiteConfig().getValue('images.postcard', { imageFileName: 'bogus-image.jpg'})
-    expect(value).toBe('https://example.com/images/postcards/bogus-image.jpg');
-})
+describe('SiteConfig', () => {
 
-test('given value a.b.c in config, return that value', () => {
-    let value = new SiteConfig().getValue('api.root');
-    expect(value).toBe('http://dummyhost.com');
-})
+    beforeEach(() => {
+        global.process.env = {
+            NEXT_PUBLIC_API_ROOT: 'http://dummyhost.com'
+        };
+    });
 
-test('find real endpoint', () => {
-    let endpoint = new SiteConfig().getEndpoint('entries.entry', { id: 1138 });
-    expect(endpoint).toEqual('http://dummyhost.com/fake-entry-endpoint/1138');
+    test('get a formatted value', () => {
+        let value = new SiteConfig().getValue('images.postcard', { imageFileName: 'bogus-image.jpg' })
+        expect(value).toBe('https://example.com/images/postcards/bogus-image.jpg');
+    })
+
+    test('given value a.b.c in config, return that value', () => {
+        let value = new SiteConfig().getValue('api.root');
+        expect(value).toBe('http://dummyhost.com');
+    })
+
+    test('find real endpoint', () => {
+        let endpoint = new SiteConfig().getEndpoint('entries.entry', { id: 1138 });
+        expect(endpoint).toEqual('http://dummyhost.com/fake-entry-endpoint/1138');
+    });
+
 });
