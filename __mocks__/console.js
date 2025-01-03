d5daf48d5cb3175cd01d08447c4c1af31a6cf8d7
craser <chris@raser.io>
2025-01-02 23:00:39 -0800

    JEST: fixing bogus setup

--- __mocks__/console.js
+++
@@ -1,8 +0,0 @@
-// __mocks__/console.js
-module.exports = {
-  log: jest.fn(),
-  warn: console.warn,
-  error: console.error,
-  info: console.info,
-  debug: console.debug,
-};
--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,9 @@
 // mock out console so we're not spewing to test logs
 global.console = {
+    ...global.console,
     log: jest.fn(),
     warn: jest.fn(),
     error: jest.fn(),
+    info: jest.fn(),
+    debug: jest.fn(),
 };
