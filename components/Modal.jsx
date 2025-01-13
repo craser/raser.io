4dc13927966694997a6d50c4e84822394ec95fe7
craser <chris@raser.io>
2025-01-12 19:22:58 -0800

    MAIN: more

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,21 +1,31 @@
 import styles from './Modal.module.scss'
 
+/**
+ * Creates a modal dialog. Dismisses on ESC or clicking outside backdrop.
+ * @param onDismiss
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Modal({ onDismiss, children }) {
-    function dismiss() {
-        try {
-            onDismiss();
-        } catch (e) {
-            console.warn('error closing Modal', e);
-        }
-    }
 
+    // Stop clicks from propagating to the backdrop - the backdrop will interpret them as dismissal.
     function dialogClicked(e) {
         e.stopPropagation();
     }
 
+    //
+    function onKeyDown(e) {
+        if (e.key === 'Escape') {
+            onDismiss();
+        }
+    }
+
     return (
-        <div className={styles.backdrop} onClick={dismiss}>
-            <div className={styles.modal} onClick={dialogClicked}>
+        <div className={styles.backdrop} onClick={function () {
+            onDismiss();
+        }} onKeyDown={onKeyDown}>
+            <div className={styles.modal} onClick={dialogClicked} onKeyDown={onKeyDown}>
                 {children}
             </div>
         </div>
