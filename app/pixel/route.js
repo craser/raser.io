efbaa4106945fff5f8de50529fc851a346b46c8d
craser <chris@raser.io>
2025-02-16 23:19:33 -0800

    MAIN: basic pixel working

--- app/pixel/route.js
+++ app/pixel/route.js
@@ -1,16 +1,25 @@
 import { NextResponse } from "next/server";
-import { init, track } from '@amplitude/analytics-node';
+import * as amplitude from '@amplitude/analytics-node';
 import { promises as fs } from 'fs';
 import path from 'path';
 import SiteConfig from "@/lib/SiteConfig";
 
 export async function GET(request) {
+    console.log(`route: ${request.url}`);
     const siteConfig = new SiteConfig();
     const apiKey = siteConfig.getValue('amplitude.apiKey');
     const options = siteConfig.getValue('amplitude.server.options');
-    init(apiKey);
+    await amplitude.init(apiKey, options);
 
-    track('pixel');
+    const response = await amplitude.track('pixel',
+        {
+            source: 'unknown',
+        },
+        {
+            user_id: 'guest',
+        }).promise;
+    console.log(`amplitude tracking response`);
+    console.log(JSON.stringify(response, null, 2));
 
     // Read the file from the public directory
     const filePath = path.join(process.cwd(), 'public', 'analytics', 'px.png');
