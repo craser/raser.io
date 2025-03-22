defaa4efe39bdbf1a526e6807d23588e5f33bd5b
craser <chris@raser.io>
2025-03-22 11:53:45 -0700

    REDESIGN: switch based on feature flag

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import FrontLandingLayout from "@/components/templates/FrontLandingLayout";
 import LatestPost from "@/components/frontpage/LatestPost";
+import { useFeatureEnabled, useFeatureFlags } from "@/components/flags/FeatureFlagProvider";
+import StandardLayout from "@/components/templates/StandardLayout";
+import LogEntries from "@/components/LogEntries";
 
-export default function Home({ initialEntries }) {
-    return (
-        <FrontLandingLayout
-            content={<LatestPost />}
-        />
-    )
+export default function Home() {
+    const flags = useFeatureFlags();
+    const showNewLandingPage = useFeatureEnabled('showLandingFrontpage')
+    const navSearchEnabled = useFeatureEnabled('navSearch');
+    console.log(`SANITY CHECK: navSearchEnabled=${navSearchEnabled}`);
+
+    if (showNewLandingPage) {
+        return (
+            <FrontLandingLayout
+                content={<LatestPost/>}
+            />
+        )
+    } else {
+        return (
+            <StandardLayout
+                content={<LogEntries initialEntries={null} initialPage={0} pageSize={30}/>}
+            />
+        );
+    }
 }
