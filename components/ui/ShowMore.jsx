73c39f49f73625a889a488279c499c7fc6d216a3
craser <chris@raser.io>
2025-03-26 22:41:31 -0700

    REDESIGN: I have now spent far too much of my life fighting with fills and svgs.

--- components/ui/ShowMore.jsx
+++ components/ui/ShowMore.jsx
@@ -1,3 +1,33 @@
+// FIXME: Replace with MUI asap.
+import styles from './ShowMore.module.scss';
+import { useRef, useState, useEffect } from 'react';
+import { ChevronUp, ChevronDown } from 'lucide-react';
+
 export default function ShowMore({ children }) {
-    return <>{children}</>
+    const [expanded, setExpanded] = useState(false);
+    const contentContainerRef = useRef(null);
+    const contentRef = useRef(null);
+
+    const expand = () => {
+        contentContainerRef.current.style.height = contentRef.current.clientHeight + 'px';
+        setExpanded(true);
+    }
+    const collapse = () => {
+        contentContainerRef.current.style.height = 0;
+        setExpanded(false);
+    }
+
+    useEffect(collapse, []);
+
+    return (
+        <>
+            {!expanded && <button className={styles.showMore} onClick={expand}><span>more</span><ChevronDown/></button>}
+            <div ref={contentContainerRef} class={styles.content}>
+                <div ref={contentRef}>
+                    {children}
+                </div>
+            </div>
+            {expanded && <button class={styles.showLess} onClick={collapse}><span>less</span><ChevronUp/></button>}
+        </>
+    );
 }
