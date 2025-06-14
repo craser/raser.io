566827fdcb93206afed92206ce8ddae6cdcf20d8
craser <chris@raser.io>
2025-06-13 20:14:17 -0700

    MAIN: next/prev buttons are still terrible, but now they're marginally less terrible

--- components/NextPrevPostLinks.jsx
+++ components/NextPrevPostLinks.jsx
@@ -1,11 +1,12 @@
 import { PostLink } from "@/components/PostLink";
 import styles from './Post.module.scss';
+import { CircleArrowLeft, CircleArrowRight} from 'lucide-react'
 
 export default function NextPrevPostLinks({ nextPost, prevPost }) {
     return (
         <div className={styles.nextPrevLinksContainer}>
-            <h1><PostLink className={styles.nextLink} post={nextPost}>NEXT</PostLink></h1>
-            <h1><PostLink className={styles.prevLink} post={prevPost}>PREV</PostLink></h1>
+            <h1><PostLink className={styles.nextLink} post={nextPost}><CircleArrowLeft className={styles.nextPrevIcon} /></PostLink></h1>
+            <h1><PostLink className={styles.prevLink} post={prevPost}><CircleArrowRight className={styles.nextPrevIcon} /></PostLink></h1>
         </div>
     );
 }
