67051a011af90a9c572dbf3ba8a380114c396396
craser <chris@raser.io>
2026-09-21 00:20:31 -0700

    REMOVE-COMMENTS-LINK: removing comments link from entry meta-info

--- components/PostMetaInfo.jsx
+++ components/PostMetaInfo.jsx
@@ -1,8 +1,6 @@
 import styles from "@/components/Post.module.scss";
 import DatePosted from "@/components/DatePosted";
-import CommentsLink from "@/components/CommentsLink";
 import PostTagsList from "@/components/PostTagsList";
-import FeatureEnabled from '@/components/flags/FeatureEnabled';
 
 export default function PostMetaInfo(props) {
     let { post } = props;
@@ -11,7 +9,6 @@ export default function PostMetaInfo(props) {
             <div data-testid="post-meta-info-details" className={styles.entrymetainfo}>
                 <PostTagsList post={post} className={styles.tagslist}/>
                 <DatePosted post={post}/>
-                <FeatureEnabled feature="postCommentCount"> • <CommentsLink post={post}/></FeatureEnabled>
             </div>
         </div>
     );
